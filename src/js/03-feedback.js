86368525ae5b8120f6e5bf256c61ccb877dd860a
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2022-11-18 21:45:00 +0100

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  formEl: document.querySelector('form'),
+  emailEl: document.querySelector('input'),
+  textareaEl: document.querySelector('textarea'),
+};
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+getInput();
+
+refs.formEl.addEventListener('submit', sendForm);
+refs.formEl.addEventListener('input', throttle(setItem, 500));
+
+function setItem(event) {
+  formData[event.target.name] = event.target.value;
+  const messege = JSON.stringify(formData);
+  localStorage.setItem(LOCALSTORAGE_KEY, messege);
+}
+
+function sendForm(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function getInput() {
+  const qwer = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (qwer) {
+    refs.emailEl.value = qwer.email;
+    refs.textareaEl.value = qwer.message;
+  }
+}
