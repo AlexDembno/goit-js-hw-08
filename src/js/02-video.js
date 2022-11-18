db818d0155d00a5630b811d41ec9878c283369a6
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2022-11-18 23:44:00 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,18 +13,9 @@ function setTime(data) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
 }
 
-// function getTime() {
-//   const setLocal = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-//   if (setLocal) {
-//     return setLocal;
-//   }
-//   // return setLocal === null ? undefined : JSON.parse(setLocal);
-// }
-
 function loadTime() {
   const setLocal = JSON.parse(localStorage.getItem('videoplayer-current-time'));
   if (setLocal) {
-    setLocal;
     player.setCurrentTime(setLocal.seconds);
   }
 }
