ad73c1ac6358eeabc5ed1a6b86ea673483d62301
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2022-11-18 11:39:31 +0100

    add first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    objekt =>
+      `<li>
+	<a class="gallery__item" href="${objekt.original}">
+		<img class="gallery__image" src="${objekt.preview}" alt="${objekt.description}" />
+	</a>
+</li>`
+  )
+  .join('');
+
+listEl.insertAdjacentHTML('afterbegin', markup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
